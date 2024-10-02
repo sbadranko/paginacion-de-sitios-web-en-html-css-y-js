683f93a5d27176672d226c91487b0bac51f7bd65
Juan Siglic Muñoz <cuentas.premium100@gmail.com>
2024-10-02 19:12:36 -0300

    Add files via upload

---
+++ siglic.js
@@ -0,0 +1,12 @@
+
+function siglic1() {
+  document.getElementById("siglic3").style.display = "block";
+  document.getElementById("siglic4").style.display = "block";
+}
+
+function siglic2() {
+ document.getElementById("siglic3").style.display = "none";
+  document.getElementById("siglic4").style.display = "none";
+}
+
+
